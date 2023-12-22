35ffea87cdb5290ce181b88b0511980374350c2c
SultanBinAR <itssultanbin@gmail.com>
2023-12-22 23:31:57 +0500

    Add project files.

---
+++ WebBulky/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBulky.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
